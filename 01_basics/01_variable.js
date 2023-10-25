57d0cd47204139b6c9189ba1b37d7fc7289cf009
Ashraf Hussain <96094419+ashrafhuss@users.noreply.github.com>
2023-10-25 20:20:39 +0000

    variable and datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const studentId=3422455;
+let studentName="Ahmed";
+var studentAddres="Lahore";
+let stundetNum;
+// studentId=43224;
+
+ studentName="Jawad";
+ var studentAddres="Islamabad capital";
+ console.table([studentId, studentName, studentAddres, stundetNum]);
