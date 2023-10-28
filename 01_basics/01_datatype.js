85b11ad17c69bd11ba94f859d5e91afb7f95b48f
Ashraf Hussain <96094419+ashrafhuss@users.noreply.github.com>
2023-10-28 07:35:36 +0000

    postfix

--- 01_basics/01_datatype.js
+++ 01_basics/01_datatype.js
@@ -1,7 +1,19 @@
 "use strict" //Treat all JS code as newer version code
-let isboolean=444;
+/* let isboolean=444;
 let conBolean=String(isboolean)
 
 console.log(typeof conBolean)
 
-console.log(conBolean);
+console.log(conBolean); */
+//-------------------------Operation----------------------
+// console.log("3"+4+6);
+// console.log(3+4+"6");
+
+let x=7;
+let y=x++;
+// console.log("The X value in postfix",x);
+console.log("The y value ",y);
+
+let a = 8;
+let b=++a;
+console.log("A value "+a+ "B value "+b);
